543bb500837c7e2926e84760b9a3afe7bf338ab4
Mukesh Tiwari <mukesh@alpenlabs.io>
2026-04-24 15:16:45 +0545

    feat/support_sp1_v6

--- g16gen/src/circuit_args.rs
+++ g16gen/src/circuit_args.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf, str::FromStr};
+use std::{fs, path::PathBuf};
 
 use g16ckt::{
     ark::{CurveGroup, ark_serialize::CanonicalDeserialize},
@@ -19,10 +19,6 @@ const INPUT_MESSAGE_LEN: usize = 36;
 /// SP1 represents groth16 proof in gnark format (128 bytes total) with an extra 4 bytes of groth16 vkey hash prepended.
 /// We do not require the 4 bytes of prepended vkey hash and as such do not consider it.
 const COMPRESSED_PROOF_SIZE: usize = 128;
-/// Size of Groth16 Verification Key in uncompressed format
-/// Can vkey use compressed or of any format as long it is properly parsed and deserialized into a structure that
-/// the binary circuit generator can use
-const UNCOMPRESSED_VK_SIZE: usize = 328;
 /// We use default proof type of GNARK
 /// This dictates how the `RunTimeData::groth16_proof` is deserialized
 const DEFAULT_PROOF_TYPE: ProofType = ProofType::GNARK;
@@ -37,12 +33,18 @@ pub(crate) struct CompileTimeData {
     /// In the context of proof generated from zkvm, groth16_vk remains same
     /// irrespective of any changes to host program
     groth16_vk_bytes: Vec<u8>,
-    /// SP1 Verification Key Hash
+    /// SP1 Verification Key Hash (public input index 1)
     /// Represents commitment of host program
     /// This field is specific to SP1 ZKVM, other zkvm's could use one or more
     /// of such fields which are known in compile time.
     /// This field is used as input by sp1's groth16 verifier, which is host program agnostic
     sp1_vkey_hash: String,
+    /// Exit code (public input index 3) - SP1 v6 constant, typically "0"
+    exit_code: String,
+    /// VK root (public input index 4) - SP1 v6 constant
+    vk_root: String,
+    /// Proof nonce (public input index 5) - SP1 v6 constant, typically "0"
+    proof_nonce: String,
 }
 
 impl CompileTimeData {
@@ -62,9 +64,18 @@ impl CompileTimeData {
         let raw_public_input: [u8; INPUT_MESSAGE_LEN] = [0; INPUT_MESSAGE_LEN];
         let proof_bytes: [u8; COMPRESSED_PROOF_SIZE] = [0; COMPRESSED_PROOF_SIZE];
 
-        let vk_bytes: [u8; UNCOMPRESSED_VK_SIZE] = self.groth16_vk_bytes.try_into().unwrap();
         let mut vk: ark_groth16::VerifyingKey<ark_bn254::Bn254> =
-            ark_groth16::VerifyingKey::deserialize_compressed_unchecked(&vk_bytes[..]).unwrap();
+            ark_groth16::VerifyingKey::deserialize_compressed_unchecked(&self.groth16_vk_bytes[..])
+                .unwrap();
+
+        // SP1 v6 has 5 public inputs with gamma_abc_g1 having 6 elements:
+        // [0]: constant term (bias)
+        // [1]: sp1_vkey_hash (compile-time constant)
+        // [2]: committed_values_digest (runtime variable - kept)
+        // [3]: exit_code (compile-time constant)
+        // [4]: vk_root (compile-time constant)
+        // [5]: proof_nonce (compile-time constant)
+        //
         // Effect of raw public inputs known in compile time (e.g. SP1_VKEY_HASH) can be embedded into the circuit
         // through vk_y0 of groth16 verification key.
         // Given:
@@ -74,11 +85,27 @@ impl CompileTimeData {
         // if k1 is a hardcoded constant, obtain vk_y0' = vk_y0 + vk_y1 * k1.
         // Run msm with this vk_y0' and the rest of the n-1 variable scalars.
         // Since, vk_y0' will be a part of compile time constants, it will be embedded in binary circuit and as such be verifiable
-        let sp1_vkey_hash = BigUint::from_str(&self.sp1_vkey_hash).unwrap();
-        let sp1_vkey_hash: ark_bn254::Fr = sp1_vkey_hash.into();
-        let sp1_vk_gamma = vk.gamma_abc_g1[0] + vk.gamma_abc_g1[1] * sp1_vkey_hash;
-        vk.gamma_abc_g1[0] = sp1_vk_gamma.into_affine();
-        let _ = vk.gamma_abc_g1.remove(1);
+        assert_eq!(
+            vk.gamma_abc_g1.len(),
+            6,
+            "expected gamma_abc_g1 to contain 1 constant term plus 5 public input coefficients"
+        );
+
+        let sp1_vkey_hash: ark_bn254::Fr = self.sp1_vkey_hash.parse::<BigUint>().unwrap().into();
+        let exit_code: ark_bn254::Fr = self.exit_code.parse::<BigUint>().unwrap().into();
+        let vk_root: ark_bn254::Fr = self.vk_root.parse::<BigUint>().unwrap().into();
+        let proof_nonce: ark_bn254::Fr = self.proof_nonce.parse::<BigUint>().unwrap().into();
+
+        // Fold constant public inputs into gamma_abc_g1[0], keep committed_values_digest coefficient
+        let committed_values_gamma = vk.gamma_abc_g1[2];
+        let folded_gamma0 = vk.gamma_abc_g1[0]
+            + vk.gamma_abc_g1[1] * sp1_vkey_hash
+            + vk.gamma_abc_g1[3] * exit_code
+            + vk.gamma_abc_g1[4] * vk_root
+            + vk.gamma_abc_g1[5] * proof_nonce;
+
+        // After folding, only committed_values_digest remains as the live public input
+        vk.gamma_abc_g1 = vec![folded_gamma0.into_affine(), committed_values_gamma];
 
         let gnark_proof_bits: Vec<bool> = {
             let gnark_proof_bits: Vec<bool> = proof_bytes
