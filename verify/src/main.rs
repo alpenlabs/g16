c6945cbada327c82105da716b24e8b974ec444c3
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2026-06-26 08:49:34 -0500

    feat: remove hardcoded `verify` path (#103)
    * Remove hardcoded `verify` path
    
    * Panic on bad argument structure

--- verify/src/main.rs
+++ verify/src/main.rs
@@ -2,5 +2,12 @@ use ckt_fmtv5_types::v5::a::reader::verify_v5a_checksum;
 
 #[monoio::main]
 async fn main() {
-    assert!(verify_v5a_checksum("g16.ckt").await.unwrap())
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("incorrect number of arguments: verify path_to_file.v5a");
+    }
+    assert!(
+        verify_v5a_checksum(args[1].clone()).await.unwrap(),
+        "checksum verification failed"
+    );
 }
